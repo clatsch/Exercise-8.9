d90a63c7bf928def2525e1647d3077613dc35a49
Cla Tschenett <design@clatschenett.com>
2016-11-06 10:11:33 +0100

    TestMensch added (without solution)

--- src/Mensch.java
+++ src/Mensch.java
@@ -1,5 +1,44 @@
-/**
- * Created by Cla on 06.11.2016.
- */
 public class Mensch {
+    private static int gesamtZahl = 0;          //Zähler auf 0 setzen
+    private int nummer;
+    private int alter;
+    private String vorname;
+    private String nachname;
+    private boolean istMaennlich;
+
+
+    // Konstruktor
+    public Mensch(int alter, String vorname, String nachname, boolean istMaennlich) {
+        nummer = ++gesamtZahl;
+        this.alter = alter;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.istMaennlich = istMaennlich;
+    }
+
+    // Getter
+    public int getAlter() {
+        return alter;
+    }
+
+    public boolean getIstMaennlich() {
+       return true;
+    }
+
+
+    // Setter
+    public void setAlter(int neuesAlter) {
+        this.alter = neuesAlter;
+    }
+
+    public boolean aelterAls(Mensch m) {
+        return (alter > m.alter);
+    }
+
+    @Override
+    public String toString() {
+
+        String gesch = "Männlich";
+        return vorname + " " + nachname + " " + alter + " " + istMaennlich;
+    }
 }
---
+++ src/TestMensch.java
@@ -0,0 +1,10 @@
+public class TestMensch {
+
+    public static void main(String[] args) {
+        Mensch m = new Mensch(80, "Hans", "Schuetz", true);
+
+        System.out.println(m);
+    }
+
+
+}
